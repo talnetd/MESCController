34c088a5e7270e686951a204f525611f668a58db
La Min Ko <lminko.lmk@gmail.com>
2020-08-22 02:59:38 +0630

    fab variant

--- config.py
+++ config.py
@@ -1,57 +1,111 @@
-# -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - TALNET
-"""
-
 import os
-from os import environ
+from flask_appbuilder.security.manager import (
+    AUTH_OID,
+    AUTH_REMOTE_USER,
+    AUTH_DB,
+    AUTH_LDAP,
+    AUTH_OAUTH,
+)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+# Your App secret key
+SECRET_KEY = "\2\1thisismyscretkey\1\2\e\y\y\h"
 
-    basedir = os.path.abspath(os.path.dirname(__file__))
+# The SQLAlchemy connection string.
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+# SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://root:password@localhost/myapp'
 
-    SECRET_KEY = "key"
+# Flask-WTF flag for CSRF
+CSRF_ENABLED = True
 
-    # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db")
+# ------------------------------
+# GLOBALS FOR APP Builder
+# ------------------------------
+# Uncomment to setup Your App name
+APP_NAME = "MESC Controller"
 
-    # For 'in memory' database, please use:
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+# Uncomment to setup Setup an App icon
+# APP_ICON = "static/img/logo.jpg"
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# The authentication type
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password()
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+AUTH_TYPE = AUTH_DB
 
-    # THEME SUPPORT
-    #  if set then url_for('static', filename='', theme='')
-    #  will add the theme name to the static URL:
-    #    /static/<DEFAULT_THEME>/filename
-    # DEFAULT_THEME = "themes/dark"
-    DEFAULT_THEME = None
-    APPLICATION_NAME = "MESC Controller"
-    USER_AUTO_LOGIN_AFTER_REGISTER = False
+# Uncomment to setup Full admin role name
+# AUTH_ROLE_ADMIN = 'Admin'
 
+# Uncomment to setup Public role name, no authentication needed
+# AUTH_ROLE_PUBLIC = 'Public'
 
-class ProductionConfig(Config):
-    DEBUG = False
+# Will allow user self registration
+# AUTH_USER_REGISTRATION = True
 
-    # Security
-    SESSION_COOKIE_HTTPONLY = True
-    REMEMBER_COOKIE_HTTPONLY = True
-    REMEMBER_COOKIE_DURATION = 3600
+# The default user self registration role
+# AUTH_USER_REGISTRATION_ROLE = "Public"
 
-    # PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
-        environ.get("APPSEED_DATABASE_USER", "appseed"),
-        environ.get("APPSEED_DATABASE_PASSWORD", "appseed"),
-        environ.get("APPSEED_DATABASE_HOST", "db"),
-        environ.get("APPSEED_DATABASE_PORT", 5432),
-        environ.get("APPSEED_DATABASE_NAME", "appseed"),
-    )
+# When using LDAP Auth, setup the ldap server
+# AUTH_LDAP_SERVER = "ldap://ldapserver.new"
 
+# Uncomment to setup OpenID providers example for OpenID authentication
+# OPENID_PROVIDERS = [
+#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+# ---------------------------------------------------
+# Babel config for translations
+# ---------------------------------------------------
+# Setup default language
+BABEL_DEFAULT_LOCALE = "en"
+# Your application default translation path
+BABEL_DEFAULT_FOLDER = "translations"
+# The allowed translation for you app
+LANGUAGES = {
+    "en": {"flag": "gb", "name": "English"},
+    # "pt": {"flag": "pt", "name": "Portuguese"},
+    # "pt_BR": {"flag": "br", "name": "Pt Brazil"},
+    # "es": {"flag": "es", "name": "Spanish"},
+    # "de": {"flag": "de", "name": "German"},
+    # "zh": {"flag": "cn", "name": "Chinese"},
+    # "ru": {"flag": "ru", "name": "Russian"},
+    # "pl": {"flag": "pl", "name": "Polish"},
+    "my": {"flag": "mm", "name": "Myanmar"},
+}
+# ---------------------------------------------------
+# Image and file configuration
+# ---------------------------------------------------
+# The file upload folder, when using models with files
+UPLOAD_FOLDER = basedir + "/app/static/uploads/"
 
-class DebugConfig(Config):
-    DEBUG = True
+# The image upload folder, when using models with images
+IMG_UPLOAD_FOLDER = basedir + "/app/static/uploads/"
 
+# The image upload url, when using models with images
+IMG_UPLOAD_URL = "/static/uploads/"
+# Setup image size default is (300, 200, True)
+# IMG_SIZE = (300, 200, True)
 
-config_dict = {"Production": ProductionConfig, "Debug": DebugConfig}
+# Theme configuration
+# these are located on static/appbuilder/css/themes
+# you can create your own and easily use them placing them on the same dir structure to override
+# APP_THEME = "bootstrap-theme.css"  # default bootstrap
+# APP_THEME = "cerulean.css"
+# APP_THEME = "amelia.css"
+APP_THEME = "cosmo.css"
+# APP_THEME = "cyborg.css"
+# APP_THEME = "flatly.css"
+# APP_THEME = "journal.css"
+# APP_THEME = "readable.css"
+# APP_THEME = "simplex.css"
+# APP_THEME = "slate.css"
+# APP_THEME = "spacelab.css"
+# APP_THEME = "united.css"
+# APP_THEME = "yeti.css"
--- run.py
+++ run.py
@@ -1,25 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-License: Commercial
-Copyright (c) 2019 - present AppSeed.us
-"""
+from app import app
 
-from flask_migrate import Migrate
-from os import environ
-from sys import exit
-
-from config import config_dict
-from app import create_app, db
-
-get_config_mode = environ.get("MESC_CONFIG_MODE", "Debug")
-
-try:
-    config_mode = config_dict[get_config_mode.capitalize()]
-except KeyError:
-    exit("Error: Invalid CONFIG_MODE environment variable entry.")
-
-app = create_app(config_mode)
-Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run()
+app.run(host="0.0.0.0", port=8080, debug=True)
