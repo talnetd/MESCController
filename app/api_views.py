148ed6f76b09e8dd3bc700ad41c18f4f202b57e8
La Min Ko <lminko.lmk@gmail.com>
2020-10-01 21:58:09 +0630

    add callback url, add bill details

--- app/api_views.py
+++ app/api_views.py
@@ -1,4 +1,7 @@
+import logging
 from http import HTTPStatus
+
+import requests
 from flask import request
 from flask_appbuilder.api import BaseApi, expose, safe
 from flask_appbuilder.security.decorators import protect
@@ -7,17 +10,20 @@ from app import db
 from app.models import Bills
 
 
+logger = logging.getLogger()
+
+
 class BillsAPI(BaseApi):
 
     resource_name = "bills"
 
-    @expose("/check", methods=["GET"])
+    @expose("/check/public", methods=["GET"])
     @safe
     def check_bill_status(self):
-        """Check bill status
+        """Check bill status. (PUBLIC API)
         ---
         get:
-          summary: Check Meter Bill Status using Reference Code.
+          summary: Check Meter Bill Status using Reference Code. Note: This is PUBLIC API.
           responses:
             200:
               description: status of Meter Bill
@@ -43,6 +49,60 @@ class BillsAPI(BaseApi):
             resp_data["is_billed"] = billed
             return self.response(HTTPStatus.OK, data=resp_data)
 
+    @expose("/check")
+    @protect()
+    @safe
+    def check_private(self):
+        """Check bill status in details. (PROTECTED API)
+        ---
+        get:
+          summary: Check Meter Bill Status using Reference Code. Note: This is PROTECTED API.
+          responses:
+            200:
+              description: status of Meter Bill
+              content:
+                application/json:
+                  schema:
+                    type: object
+                    properties:
+                      data:
+                        type: object
+                        properties:
+                          is_billed:
+                            type: boolean
+                          previous_reading:
+                            type: integer
+                          current_reading:
+                            type: integer
+                          diff_reading:
+                            type: integer
+                          sub_total:
+                            type: float
+                          maintenance_fee:
+                            type: float
+                          horsepower_fee:
+                            type: float
+                          grand_total:
+                            type: float
+        """
+        resp_data = dict()
+        if request.method == "GET":
+            billed = False
+            ref_code = request.args.get("ref_code")
+            found = Bills.find_by_ref_code(ref_code)
+            if found:
+                billed = found.is_billed or False
+                resp_data["previous_reading"] = found.previous_reading
+                resp_data["current_reading"] = found.current_reading
+                resp_data["diff_reading"] = found.diff_reading
+                resp_data["sub_total"] = found.sub_total
+                resp_data["maintenance_fee"] = found.maintenance_fee
+                resp_data["horsepower_fee"] = found.horsepower_fee
+                resp_data["grand_total"] = found.grand_total
+
+            resp_data["is_billed"] = billed
+            return self.response(HTTPStatus.OK, data=resp_data)
+
     @expose("/pay", methods=["POST"])
     @protect()
     @safe
@@ -60,6 +120,10 @@ class BillsAPI(BaseApi):
                   properties:
                     ref_code:
                       type: string
+                    callback_url:
+                      type: string
+                    callback_data:
+                      type: object
 
           responses:
             200:
@@ -93,14 +157,38 @@ class BillsAPI(BaseApi):
         message = ""
         status = HTTPStatus.BAD_REQUEST
         if request.method == "POST":
+            # Sample: using form data.
             # print(request.form)
-            ref_code = request.form.get("ref_code")
+            # ref_code = request.form.get("ref_code")
+
+            # Sample: using application/json
+            json_data = request.get_json() or dict()
+            ref_code = json_data.get("ref_code")
+            cb_url = json_data.get("callback_url")
+            cb_data = json_data.get("callback_data")
+
             found = Bills.find_by_ref_code(ref_code)
             if found:
                 if not found.is_billed:
                     found.is_billed = True
                     db.session.add(found)
                     db.session.commit()
+
+                    try:
+                        resp = requests.post(cb_url, data=cb_data)
+                        if not resp.ok:
+                            logger.warning(
+                                f"An error occurred when executing callback for {ref_code}."
+                            )
+                            logger.error(resp.text)
+                        else:
+                            logger.info(
+                                f"Successfully executed callback URL for {ref_code}"
+                            )
+                            logger.info(resp.text)
+                    except Exception as exc:
+                        logger.warning("Could not execute callback API.")
+                        logger.error(exc)
 
                     status = HTTPStatus.OK
                     message = f"Meter Bill has been paid successfully."
